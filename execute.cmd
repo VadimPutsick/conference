5e388dfc07ecf64f66e10ee52a590891e92082fb
Putsick Vadim <trewyut@mail.ru>
2019-04-05 23:41:29 +0300

    Add all sample date for conference app

--- execute.cmd
+++ execute.cmd
@@ -1,2 +1,3 @@
-sfdx force:data:tree:import --sobjecttreefiles data/Venue__c.json -u test-vijnrdrrozki@example.com^
- & sfdx force:data:tree:import --sobjecttreefiles data/Conference__c-Attendee__c.json -u test-vijnrdrrozki@example.com
+sfdx force:data:tree:import --sobjecttreefiles data/Conference__c-Participant__c-Feedback__c-Session__c.json -u test-vijnrdrrozki@example.com^
+ & sfdx force:data:tree:import --sobjecttreefiles data/Hotel__c-Booking__c.json -u test-vijnrdrrozki@example.com^
+ & sfdx force:data:tree:import -f data/Conference__c-Participant__c-Feedback__c-Session__c.json -u test-vijnrdrrozki@example.com
