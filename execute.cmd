33fccfa330d55ea8acda97e01bbb4d8627222bc4
Putsick Vadim <trewyut@mail.ru>
2019-04-05 23:50:37 +0300

    Add ecxecute command for importing data

--- execute.cmd
+++ execute.cmd
@@ -1,3 +1,5 @@
-sfdx force:data:tree:import --sobjecttreefiles data/Conference__c-Participant__c-Feedback__c-Session__c.json -u test-vijnrdrrozki@example.com^
- & sfdx force:data:tree:import --sobjecttreefiles data/Hotel__c-Booking__c.json -u test-vijnrdrrozki@example.com^
- & sfdx force:data:tree:import -f data/Conference__c-Participant__c-Feedback__c-Session__c.json -u test-vijnrdrrozki@example.com
+sfdx force:data:tree:import --sobjecttreefiles data/Hotel__c.json -u test-vijnrdrrozki@example.com^
+ & sfdx force:data:tree:import --sobjecttreefiles data/Interviewer__c.json -u test-vijnrdrrozki@example.com^
+ & sfdx force:data:tree:import --sobjecttreefiles data/Speaker__c.json -u test-vijnrdrrozki@example.com^
+ & sfdx force:data:tree:import --sobjecttreefiles data/Venue__c.json -u test-vijnrdrrozki@example.com^
+ & sfdx force:data:tree:import --sobjecttreefiles data/Conference__c-Participant__c-Feedback__c-Session__c.json -u test-vijnrdrrozki@example.com
