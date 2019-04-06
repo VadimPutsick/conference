9860c0d36e80752088a73ae60466048cf4c1f255
Putsick Vadim <trewyut@mail.ru>
2019-04-06 17:32:25 +0300

    Change org for data filling to default

--- execute.cmd
+++ execute.cmd
@@ -1,8 +1,8 @@
 sfdx force:org:create -s -f config/project-scratch-def.json -a conference-final^
  & sfdx force:user:create --setalias qa-user --definitionfile config/user-def.json^
  & sfdx force:source:push^
- & sfdx force:data:tree:import --sobjecttreefiles data/Hotel__c.json -u test-vijnrdrrozki@example.com^
- & sfdx force:data:tree:import --sobjecttreefiles data/Interviewer__c.json -u test-vijnrdrrozki@example.com^
- & sfdx force:data:tree:import --sobjecttreefiles data/Speaker__c.json -u test-vijnrdrrozki@example.com^
- & sfdx force:data:tree:import --sobjecttreefiles data/Venue__c.json -u test-vijnrdrrozki@example.com^
- & sfdx force:data:tree:import --sobjecttreefiles data/Conference__c-Participant__c-Feedback__c-Session__c.json -u test-vijnrdrrozki@example.com
+ & sfdx force:data:tree:import --sobjecttreefiles data/Hotel__c.json^
+ & sfdx force:data:tree:import --sobjecttreefiles data/Interviewer__c.json^
+ & sfdx force:data:tree:import --sobjecttreefiles data/Speaker__c.json^
+ & sfdx force:data:tree:import --sobjecttreefiles data/Venue__c.json^
+ & sfdx force:data:tree:import --sobjecttreefiles data/Conference__c-Participant__c-Feedback__c-Session__c.json
